59856a7f58c2e781da8d859931f4cfb43c5895c2
Santiago Herrera <150970732+liquidsnk86@users.noreply.github.com>
2025-01-08 12:08:08 -0300

    Create get-location.js

---
+++ get-location.js
@@ -0,0 +1,19 @@
+export default function getWeatherData() {
+  let coords = {
+    lat: String | Number,
+    lon: String | Number,
+  };
+  
+  if (navigator.geolocation) {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        coords.lat = pos.coords.latitude;
+        coords.lon = pos.coords.longitude;
+        resolve(coords);
+        if (!pos.coords) {
+          reject(new Error("Error al obteber la geolocalización"));
+        }
+      });
+    });
+  }
+}
